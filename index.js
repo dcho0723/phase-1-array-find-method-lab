a203dbdfb9a6ccb9523b91462f471e6df4f18680
dcho0723 <dcho0723@yahoo.com>
2021-06-30 16:40:13 -0400

    commit tree 77ad4b3c356641abf8561476fab8b7ae5200cff5

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+    const foundSeason = record.find(function(objArr) {
+        return objArr.result === "W"
+    })
+    if (foundSeason) {
+        return foundSeason.year
+    }
+}
